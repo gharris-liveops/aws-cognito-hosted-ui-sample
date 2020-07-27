f2a8b9231ff836685975092432572c5f60a65e29
Greg Harris <gpharris@amazon.com>
2020-07-27 16:52:48 -0500

    change redirect to custom domain

--- src/assets/pool-config.js
+++ src/assets/pool-config.js
@@ -4,7 +4,7 @@ const pool = {
     CLIENTID: '1pk2jfeq53lht0mlt27r570ot6',
     HOSTED_UI: 'https://auth0.demo.octank.awsdemo.link',
     REGION: 'us-east-1',
-    HTTPS_REDIRECT_GW_URI: 'https://master.d1azaji2pa6oqv.amplifyapp.com/done'
+    HTTPS_REDIRECT_GW_URI: 'https://redirect.octank.awsdemo.link/done'
 }
 
 module.exports = pool;
