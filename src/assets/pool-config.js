9b4ce38ff3d9197b48807218b9fe592181dfa763
Harris <gpharris@macbook-pro.lan>
2020-07-27 16:44:20 -0500

    change redirect to amplify url

--- src/assets/pool-config.js
+++ src/assets/pool-config.js
@@ -4,7 +4,7 @@ const pool = {
     CLIENTID: '1pk2jfeq53lht0mlt27r570ot6',
     HOSTED_UI: 'https://auth0.demo.octank.awsdemo.link',
     REGION: 'us-east-1',
-    HTTPS_REDIRECT_GW_URI: 'https://redirect.octank.awsdemo.link/done'
+    HTTPS_REDIRECT_GW_URI: 'https://master.d1azaji2pa6oqv.amplifyapp.com/done'
 }
 
 module.exports = pool;
